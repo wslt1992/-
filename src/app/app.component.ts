d9ce25739fc884914b8906df0fdc1bf65a8a1074
liu tao <465275024@qq.com>
2018-07-27 15:01:06 +0800

    1加入animate动画2.加入wow进入后执行动画

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import 'hammerjs';
+import {WOW} from '../assets/js/wow/wow.js'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    new WOW().init();
+  }
   title = 'app';
 }
